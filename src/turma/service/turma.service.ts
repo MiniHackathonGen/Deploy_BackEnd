ccd69fe716ef168454d140d2000f725a210fa82e
MiniHackathonGen <122381866+MiniHackathonGen@users.noreply.github.com>
2023-01-11 09:39:34 -0300

    Update turma.service.ts
    consertando um T maiúsculo em "Turma"

--- src/turma/service/turma.service.ts
+++ src/turma/service/turma.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { DeleteResult, Repository } from "typeorm";
-import { Turmas } from "../entities/Turma.entity";
+import { Turmas } from "../entities/turma.entity";
 
 @Injectable()
 export class TurmaService {
